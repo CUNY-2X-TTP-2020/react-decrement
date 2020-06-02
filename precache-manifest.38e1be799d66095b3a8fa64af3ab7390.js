11bf61d36eabb543d9c71c5ea929e50f09da84e0
RickyC0626 <rickychon99@gmail.com>
2020-06-02 16:58:34 -0400

    Updates

---
+++ precache-manifest.38e1be799d66095b3a8fa64af3ab7390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5add1c2178255ace93c2373f10916e0",
+    "url": "/CUNY-2X-TTP-2020/react-decrement/index.html"
+  },
+  {
+    "revision": "87e23735898bc04a2f6b",
+    "url": "/CUNY-2X-TTP-2020/react-decrement/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3df96dd1d203977c78d",
+    "url": "/CUNY-2X-TTP-2020/react-decrement/static/js/2.9c681bd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CUNY-2X-TTP-2020/react-decrement/static/js/2.9c681bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e23735898bc04a2f6b",
+    "url": "/CUNY-2X-TTP-2020/react-decrement/static/js/main.b0d3c43d.chunk.js"
+  },
+  {
+    "revision": "cd609a5355b92e6da8dc",
+    "url": "/CUNY-2X-TTP-2020/react-decrement/static/js/runtime-main.f2b8e986.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CUNY-2X-TTP-2020/react-decrement/static/media/logo.5d5d9eef.svg"
+  }
+]);
