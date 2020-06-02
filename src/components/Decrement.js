75bdec1fc2440bd5426c5f134c7e49a528e50f39
RickyC0626 <rickychon99@gmail.com>
2020-06-02 17:26:35 -0400

    Add start prop for number to begin decrementing from

--- src/components/Decrement.js
+++ src/components/Decrement.js
@@ -6,10 +6,22 @@ class Decrement extends Component
     constructor(props)
     {
         super(props);
+        this.state = { number: props.start };
+    }
+
+    render()
+    {
+        return (
+            <>
+                
+            </>
+        )
     }
 }
 
 Decrement.propTypes = 
 {
-
+    start: PropTypes.number.isRequired
 }
+
+export default Decrement;
