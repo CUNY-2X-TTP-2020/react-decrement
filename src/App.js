72b9de76e286fb183643d16a7c33c11b7bfc38bd
RickyC0626 <rickychon99@gmail.com>
2020-06-02 17:31:18 -0400

    Add decrement component on homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Decrement from './components/Decrement';
+
 function App() 
 {
     return (
         <div className="App">
         <header className="App-header">
             <h1>Decrement</h1>
+            <Decrement start={20} />
         </header>
         </div>
     );
