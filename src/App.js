8f30ed37596bafc0154b533ca6071a5ff8828985
RickyC0626 <rickychon99@gmail.com>
2020-06-02 20:01:03 -0400

    Add one more Decrement component example

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App()
             <Decrement start={20} amount={1} />
             <Decrement start={50} amount={5} />
             <Decrement start={100} amount={10} />
+            <Decrement start={2020} amount={199} />
         </header>
         </div>
     );
